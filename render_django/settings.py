1caa3e75f6bdaff70c148590fe351628a362b3a2
Mizuhara Itsuki <110594150+Iccyan21@users.noreply.github.com>
2023-12-15 13:28:10 +0900

    いつになったらできるの？

--- render_django/settings.py
+++ render_django/settings.py
@@ -138,7 +138,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    'https://react-jbl4.onrender.com',  # Reactアプリのオリジン
+    'http://localhost:3000',
     # フロントが必須
 ]
 
