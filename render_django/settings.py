9f419cc96a821b3ef2188314403d08bfe471541f
Mizuhara Itsuki <110594150+Iccyan21@users.noreply.github.com>
2023-12-15 13:53:55 +0900

    マジでちゃんとしろお前

--- render_django/settings.py
+++ render_django/settings.py
@@ -135,15 +135,6 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ALLOW_ALL_ORIGINS = True
-
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'https://fuck-render-react.onrender.com'
-    # フロントが必須
-]
-
-
 # imageやmovieの保存先
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
